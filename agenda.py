fc7884e027a118652a1949271830ca03d52611c9
Juliena <juliena456@gmail.com>
2025-07-16 20:50:39 +0200

    Now, when you open the agenda, it starts in the current mouth

--- agenda.py
+++ agenda.py
@@ -4,11 +4,31 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.clock import Clock
+
+import calendar
+from datetime import date, datetime
 
 Builder.load_file("AgendaWidget.kv")
 
 class AgendaWidget(TabbedPanel):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.select_current_month()
+        Clock.schedule_once(self.select_current_month, 0)
+
+    def select_current_month(self, *args):
+        month_index = datetime.now().month  
+        month_ids = [
+            "jan_tab", "feb_tab", "mar_tab", "apr_tab", "may_tab", "jun_tab",
+            "jul_tab", "aug_tab", "sep_tab", "oct_tab", "nov_tab", "dec_tab"
+        ]
+
+        current_month_id = month_ids[month_index - 1]
+        if current_month_id in self.ids:
+            self.switch_to(self.ids[current_month_id])
+        else:
+            print(f"Warning: {current_month_id} not found in ids")
 
 class agenda(App):
     def build(self):

19343c0358cc9c03dc3a76ce749d4f9396ad9ab2
Juliena <juliena456@gmail.com>
2025-07-17 14:05:14 +0200

    Now we have the id of the first and current day ! So we can start the counterto place days in the tab and the current day it's to have an effect when we open the calender

---
+++ calendar_gestion.py
@@ -0,0 +1,29 @@
+import calendar
+from datetime import date, datetime
+
+import re 
+
+def FirstDayInAMouth():
+    current_datetime = datetime.now()
+    current_year = current_datetime.year
+    current_month = current_datetime.month + 1
+
+    first_day_index = calendar.monthrange(current_year, current_month)[0]
+    print(f"Id of the first_day_index = {first_day_index}") # 0 = monday, 1 = tuesday ,ects...
+    
+    return first_day_index, current_month
+
+def CurrentDayId(first_day_index, current_month):
+    current_day = datetime.now().day
+
+    month_abbr = calendar.month_abbr[current_month].lower()
+    current_day_id = f"{month_abbr}_{first_day_index}_btn"
+
+    print(f"Id of the current day = {current_day_id}")
+
+    return current_day_id 
+
+first_day_index, current_month = FirstDayInAMouth()
+CurrentDayId(first_day_index, current_month)
+
+#def Init_agenda(first_day_name, current_month):

a2428b042c7dff7e30042f22c6d2218453001464
Juliena <juliena456@gmail.com>
2025-07-17 14:17:32 +0200

    Correction of the logic for current_day_id, now it's : 'index_in_tab = first_day_index + current_day - 1'

--- calendar_gestion.py
+++ calendar_gestion.py
@@ -6,7 +6,7 @@ import re
 def FirstDayInAMouth():
     current_datetime = datetime.now()
     current_year = current_datetime.year
-    current_month = current_datetime.month + 1
+    current_month = current_datetime.month
 
     first_day_index = calendar.monthrange(current_year, current_month)[0]
     print(f"Id of the first_day_index = {first_day_index}") # 0 = monday, 1 = tuesday ,ects...
@@ -17,11 +17,12 @@ def CurrentDayId(first_day_index, current_month):
     current_day = datetime.now().day
 
     month_abbr = calendar.month_abbr[current_month].lower()
-    current_day_id = f"{month_abbr}_{first_day_index}_btn"
+    index_in_tab = first_day_index + current_day - 1
+    current_day_id = f"{month_abbr}_{index_in_tab}_btn"
 
     print(f"Id of the current day = {current_day_id}")
 
-    return current_day_id 
+    return current_day_id
 
 first_day_index, current_month = FirstDayInAMouth()
 CurrentDayId(first_day_index, current_month)
